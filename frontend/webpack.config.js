5863c4d0e61e32a484fcb2d75ae61f01592dae77
Gregory Sims <briggybros.2@googlemail.com>
2018-01-27 22:32:02 +0000

    Initial commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.jsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    fix: true,
+                    cache: true,
+                },
+            },
+            {
+                test: /\.(js|jsx)?$/,
+                loader: 'babel-loader?cacheDirectory',
+                exclude: path.resolve(__dirname, '..', 'node_modules'),
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin([{
+            from: path.join(__dirname, 'src', 'static'),
+            to: path.join(__dirname, 'dist'),
+        }]),
+    ],
+};
---
+++ server/index.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+const DIST = path.join(__dirname, '..', 'frontend', 'dist');
