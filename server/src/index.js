3efe1f8469c4da3fab91b1ea976057db1aeec75b
Gregory Sims <briggybros.2@googlemail.com>
2018-02-08 12:49:24 +0000

    Set up basic neo4j database

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,10 @@
 import path from 'path';
 import express from 'express';
 
-import * as database from './database';
-
 const PORT : number | string = process.env.PORT || 8080;
 const DIST : string = path.join(__dirname, '..', '..', 'frontend', 'dist');
 
 const app = express();
-database.init();
 
 function xhtmlify(req, res, done) {
     if (req.accepts('application/xhtml+xml')) {
@@ -28,15 +25,3 @@ app.get('*', xhtmlify, (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
-function exitHandler(err) {
-    if (err) console.error(err);
-    database.close();
-    process.exit();
-}
-
-process.on('exit', exitHandler);
-process.on('SIGINT', exitHandler);
-process.on('SIGUSR1', exitHandler);
-process.on('SIGUSR2', exitHandler);
-process.on('uncaughtException', err => exitHandler(err));
