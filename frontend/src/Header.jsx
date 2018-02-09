13a50b3c2aa7f357d60790427b1f9d9cc9b3f0b4
Kyle <kyle.jake.kw@gmail.com>
2018-02-09 11:41:17 +0000

    flow is being a bitch about render not being a thing but it is :(

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -5,5 +5,21 @@ import styled from 'styled-components';
 const Navigation = styled.nav`
     width: 100%;
     height: 56px; /* TODO: alter for screen size*/
-
+    /*theme dependent*/
+    background-color: ${props => props.theme.primary};
 `;
+
+Navigation.defaultProps = {
+    theme:{
+        primary: '#e9304a'
+    }
+};
+
+const Container = styled.div`
+    display: flex;
+    max-width:90%;
+`;
+
+render(
+    
+);
