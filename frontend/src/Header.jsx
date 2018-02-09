fc7ab059d25c6b07f9b9df4e49c89a36e73fa93e
Kyle <kyle.jake.kw@gmail.com>
2018-02-09 10:01:55 +0000

    an absolute mess

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,9 @@
+// @flow
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Navigation = styled.nav`
+    width: 100%;
+    height: 56px; /* TODO: alter for screen size*/
+
+`;
