599ff2454912e26a3c661c1a14be65a175e030bf
Gregory Sims <briggybros.2@googlemail.com>
2018-02-12 18:32:59 +0000

    Added fonts and colours

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,54 @@
 // @flow
 import React from 'react';
 import { render } from 'react-dom';
+import { injectGlobal, ThemeProvider } from 'styled-components';
+
+injectGlobal`
+    @font-face {
+        font-family: 'bebas neue bold';
+        src: url('/bebasneue_bold-webfont.woff2') format('woff2'),
+             url('/bebasneue_bold-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    
+    @font-face {
+        font-family: 'bebas neue light';
+        src: url('/bebasneue_light-webfont.woff2') format('woff2'),
+             url('/bebasneue_light-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    @import url('https://fonts.googleapis.com/css?family=Raleway');
+
+    body {
+        margin: 0;
+        font-family: 'Raleway', sans-serif;
+    }
+
+    h1, h2, h3, h4 h5, h6 {
+        font-family: 'bebas neue bold', sans-serif;
+    }
+`;
+
+const theme = {
+    colors: {
+        primary: '#ea504b',
+        white: '#fff',
+        black: '#242020',
+    },
+};
 
 const mount : HTMLElement | null = document.getElementById('app');
 
 if (mount) {
     render(
-        <div />,
+        <ThemeProvider
+            theme={theme}
+        >
+            <div />
+        </ThemeProvider>,
         mount,
     );
 } else {
