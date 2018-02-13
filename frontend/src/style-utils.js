b41761278e6542900a9e5285bca1c103e9a3cda3
Kyle <kyle.jake.kw@gmail.com>
2018-02-13 14:51:01 +0000

    ahhhh :fu: media queries

--- frontend/src/style-utils.js
+++ frontend/src/style-utils.js
@@ -2,26 +2,11 @@
 import styled from 'styled-components';
 import { css } from 'styled-components';
 
-const sizes = {
-    giant: 1170,
-    desktop: 992,
-    tablet: 768,
-    phablet: 572,
-    phone: 376
-  }
-  
-export const media = Object.keys(sizes).reduce((accumulator, label) => {
-    accumulator[label] = (...args: any[]) => css`
-      @media (max-width: ${sizes[label]}px) {
-        ${css(...args)}
-      }
-    `
-    return accumulator
-  }, {})
-
 export const Container = styled.div`
-    width:80%;
-    ${media.phablet`width: 95%;`}
+    width:100%;
     max-width:1280px;
     margin: 0 auto;
+    @media all and (min-width: 768px) {
+        width:80%;
+    }
 `;
