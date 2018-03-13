5c1477e8b2d5c6dffa4532a606dd0015110b03db
Gregory Sims <briggybros.2@googlemail.com>
2018-03-13 11:40:31 +0000

    Removed flow-typed folder

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,43 @@
 // @flow
 import knex from 'knex';
+import containerized from 'containerized';
 
-const db = knex({
-    client: 'mysql',
-});
+import { PromiseReduce } from './util';
+
+export default function init() : Promise<knex> {
+    const db = knex({
+        client: 'mysql',
+        connection: {
+            host: containerized() ? 'database' : process.env.DB_URL || 'localhost',
+            user: process.env.DB_USER || 'rag',
+            password: process.env.DB_PASSWORD || 'rag',
+            database: process.env.DB_DATABASE || 'rag',
+        },
+    });
+
+    return new Promise((resolve, reject) => {
+        resolve(PromiseReduce([
+            db.schema.createTableIfNotExists('users', (table) => {
+                table.increments('user_id').notNullable().unsigned().primary();
+                table.string('email');
+                table.string('name');
+                table.string('password', 60);
+            }),
+            db.schema.createTableIfNotExists('roles', (table) => {
+                table.increments('role_id').notNullable().unsigned().primary();
+                table.string('name');
+            }),
+            db.schema.createTableIfNotExists('user_roles', (table) => {
+                table.integer('user_id').unsigned().notNullable();
+                table.integer('role_id').unsigned().notNullable();
+                table.foreign('user_id').references('user_id').inTable('users');
+                table.foreign('role_id').references('role_id').inTable('roles');
+            }),
+            db.schema.createTableIfNotExists('pages', (table) => {
+                table.string('page_path').notNullable().primary();
+                table.json('content').notNullable();
+                table.json('meta');
+            }),
+        ]).catch(reject));
+    }).then(() => db);
+}
---
+++ server/src/util.js
@@ -0,0 +1,8 @@
+export function PromiseReduce(promises : Array<Promise<any>>) : Promise<any> {
+    return promises.reduce(
+        (chain, promise) => chain.then(() => Promise.resolve(promise)),
+        Promise.resolve(),
+    );
+}
+
+export const placeholder = '';
