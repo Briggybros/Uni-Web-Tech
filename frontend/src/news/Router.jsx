043d37e5d2b74998c0bed4acb8566c0a3d8bc8a3
Gregory Sims <briggybros.2@googlemail.com>
2018-04-04 18:13:26 +0100

    Work towards feed

--- frontend/src/news/Router.jsx
+++ frontend/src/news/Router.jsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
-import News from './News';
-import Editor from '../editor/Editor';
+import Feed from './Feed';
 
 export default () => (
     <Switch>
-        <Route exact path="/" component={News} />
-        {/* <Route path="/news/:id" component={Post} /> */}
-        <Route path="/news/:id/edit" component={Editor} />
-        <Route path="/news/new" component={Editor} />
+        <Route exact path="/" component={Feed} />
     </Switch>
 );
 
--- server/src/api/news.js
+++ server/src/api/news.js
@@ -1,9 +1,17 @@
 // @flow
 import { Router } from 'express';
-// import type { $Request, $Response } from 'express';
+import type { $Request, $Response } from 'express';
 
-// import News from '../models/news';
+import News from '../models/news';
 
 const newsRouter = Router();
+
+newsRouter.get('/', (req: $Request, res: $Response) => {
+    const num = req.query.num || 10;
+    const offset = req.query.offset || 0;
+    News.getArticles(num, offset).then((articles) => {
+        res.send(articles.map(article => article.toJSON()));
+    });
+});
 
 export default newsRouter;
