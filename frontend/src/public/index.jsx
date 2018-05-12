e171f4b4c44314fbea0bb122a2b06660bffa2a45
Gregory Sims <briggybros.2@googlemail.com>
2018-05-12 14:43:02 +0100

    Fixed public route

--- frontend/src/public/index.jsx
+++ frontend/src/public/index.jsx
@@ -1,6 +1,33 @@
 // @flow
 import * as React from 'react';
+import { Route } from 'react-router';
+import styled from 'styled-components';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import NewsFeed from './news/Feed';
+
+import StaticPageLoader from './StaticPageLoader';
+
+const Page = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
 
 export default () => (
-    <div />
+    <Page>
+        <Header />
+        <Route
+            exact
+            path="/"
+            component={NewsFeed}
+        />
+        <Route
+            path="/:id"
+            component={StaticPageLoader}
+        />
+        <Footer />
+    </Page>
 );
