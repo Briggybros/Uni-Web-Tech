78861777afa7401eefd9d1a41da722e3068ef8f5
Gregory Sims <briggybros.2@googlemail.com>
2018-05-12 15:05:45 +0100

    Remove broken Feed page

--- frontend/src/public/news/Feed.jsx
+++ frontend/src/public/news/Feed.jsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import PostSummary from './PostSummary';
-import Authed from '../../auth/Authed';
 import { updatePosts } from './reducer';
 
 type Props = {
@@ -11,26 +10,9 @@ type Props = {
     updatePosts: (Array<Object>, number) => void,
 }
 
-class News extends React.Component<Props> {
-    componentWillMount() {
-        fetch('/api/news')
-            .then(response => response.json())
-            .then(json => this.props.updatePosts(json.posts, json.start));
-    }
-
-    render() {
-        return (
-            <div>
-                <Authed>
-                    <button>
-                        New Post
-                    </button>
-                </Authed>
-                {this.props.posts.map(post => <PostSummary key={post.id} post={post} />)}
-            </div>
-        );
-    }
-}
+const News = (props: Props) => (
+    <div />
+);
 
 function mapStateToProps(state: Object): Object {
     return {
