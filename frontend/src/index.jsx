19238931799aa82749f6da964d2d8317521e5b63
Gregory Sims <briggybros.2@googlemail.com>
2018-05-12 20:06:26 +0100

    Add redux devtools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -11,7 +11,12 @@ import Routes from './routes';
 
 import reducer from './reducer';
 
-const store = createStore(reducer, {});
+const store = createStore(
+    reducer,
+    {},
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 injectGlobalStyles();
 
