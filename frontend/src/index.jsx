71696c0e01495129503e36b7aa131bc540d2f18f
Gregory Sims <briggybros.2@googlemail.com>
2018-05-13 14:26:50 +0100

    Fixed flowtype for redux-persist

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider as ReduxProvider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import ThemeProvider from './providers/ThemeProvider';
 import { theme, injectGlobalStyles } from './style-utils';
@@ -11,8 +14,13 @@ import Routes from './routes';
 
 import reducer from './reducer';
 
+const persistedReducer = persistReducer({
+    key: 'root',
+    storage,
+}, reducer);
+
 const store = createStore(
-    reducer,
+    persistedReducer,
     {},
     // eslint-disable-next-line no-underscore-dangle
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
@@ -27,11 +35,13 @@ if (mount) {
         <ReduxProvider
             store={store}
         >
-            <ThemeProvider
-                theme={theme}
-            >
-                <Routes />
-            </ThemeProvider>
+            <PersistGate persistor={persistStore(store)}>
+                <ThemeProvider
+                    theme={theme}
+                >
+                    <Routes />
+                </ThemeProvider>
+            </PersistGate>
         </ReduxProvider>,
         mount,
     );
