80182d732896091c2ccd24b3974b6d4502ed23a3
Kyle <kyle.jake.kw@gmail.com>
2018-05-13 14:49:28 +0100

    remove yarn.lock from gitignore, semi-completed events styles, still got some bugs but you can see where it's going

--- frontend/src/events/Timeline.jsx
+++ frontend/src/events/Timeline.jsx
@@ -1,9 +1,15 @@
 // @flow
 import * as React from 'react';
 import styled from 'styled-components';
-import { Container } from '../style-utils';
+import { Container as StandardContainer } from '../style-utils';
+
+const Container = StandardContainer.extend`
+    margin: 1rem, 0;
+`;
 
 const TimeLineContainer = styled.div`
+    position: relative;
+    padding: 3rem, 0rem;
     &:before {
         content: "";
         width: 4px;
@@ -12,12 +18,42 @@ const TimeLineContainer = styled.div`
         position: absolute;
         top: 0;
     }
-    position: relative;
-    padding: 1rem, 1rem;
+    @media (min-width: 641px){
+        left:30px;
+    }
+    @media (max-width: 640px){
+        left:0;
+    }
 `;
 
 const TimelineEventEntry = styled.div`
-
+    position: relative;
+    &:not(:last-of-type){
+        margin-bottom: 30px;
+    }
+    &:before {
+        content: "";
+        width: 100%;
+        height: 2px;
+        background-color: ${props => props.theme.colours.primary};
+        position: absolute;
+        left: 0;
+        z-index: -1;
+    }
+    @media (min-width: 641px){
+        padding-left:80px;
+        &:before {
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
+    @media (max-width: 640px){
+        padding-top:70px;
+        padding-left:20px;
+        &:before {
+            top:90px;
+        }
+    }
 `;
 
 const TimelineMonthEntry = styled.div`
@@ -29,13 +65,51 @@ const TimelineMonthEntry = styled.div`
     top: 0;
 `;
 
-const Date = styled.div``;
+const TimelineGroup = styled.div`
+    position: relative;
+    &:not(:first-of-type){
+        margin-top: 4rem;
+    }
+    @media (min-width: 641px){
+        padding-top:55px;
+    }
+    @media (max-width: 640px){
+        padding-top:40px;
+    }
+`;
 
-const Day = styled.span``;
+const Date = styled.div`
+    min-width: 65px;
+    position: absolute;
+    left: 0;
+    box-sizing: border-box;
+    padding: .5rem 1.5rem;
+    text-align: center;
+    background-color: ${props => props.theme.colours.primary};
+    color: white;
+    @media (min-width: 641px){
+        top:50%;
+        margin-top: -27px;
+    }
+    @media (max-width: 640px){
+        top:0;
+    }
+`;
 
-const Month = styled.span``;
+const Day = styled.span`
+    font-size: 2rem;
+    font-weight: 700;
+    display: block;
+`;
+
+const Month = styled.span`
+    display: block;
+    font-size: .8em;
+    text-transform: uppercase;
+`;
 
 const Post = styled.div`
+    margin-left: 10px;
     border-left: 3px solid ${props => props.theme.colours.primary};
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .12), 0 1px 2px 0 rgba(0, 0, 0, .24);
     padding: 1.5rem 2rem;
@@ -49,26 +123,30 @@ const Title = styled.h2``;
 export default () => (
     <Container>
         <TimeLineContainer>
-            <TimelineMonthEntry>
-                June 2018
-            </TimelineMonthEntry>
-            <TimelineEventEntry>
-                <Date><Day>10</Day><Month>June</Month></Date>
-                <Post>
-                    <Title>Charity Event</Title>
-                    <Content>Event thing</Content>
-                </Post>
-            </TimelineEventEntry>
-            <TimelineMonthEntry>
-                June 2018
-            </TimelineMonthEntry>
-            <TimelineEventEntry>
-                <Date><Day>10</Day><Month>June</Month></Date>
-                <Post>
-                    <Title>Charity Event</Title>
-                    <Content>Event thing</Content>
-                </Post>
-            </TimelineEventEntry>
+            <TimelineGroup>
+                <TimelineMonthEntry>
+                    June 2018
+                </TimelineMonthEntry>
+                <TimelineEventEntry>
+                    <Date><Day>10</Day><Month>June</Month></Date>
+                    <Post>
+                        <Title>Charity Event</Title>
+                        <Content>Event thing</Content>
+                    </Post>
+                </TimelineEventEntry>
+            </TimelineGroup>
+            <TimelineGroup>
+                <TimelineMonthEntry>
+                    June 2018
+                </TimelineMonthEntry>
+                <TimelineEventEntry>
+                    <Date><Day>10</Day><Month>June</Month></Date>
+                    <Post>
+                        <Title>Charity Event</Title>
+                        <Content>Event thing</Content>
+                    </Post>
+                </TimelineEventEntry>
+            </TimelineGroup>
         </TimeLineContainer>
     </Container>
 );
